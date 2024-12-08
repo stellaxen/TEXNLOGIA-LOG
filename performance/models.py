5379bf9b91b1143af90f08da3c2ea65b67b17580
stellaxen <stella.xenaki9@gmail.com>
2024-12-08 21:41:29 +0200

    Manager and rating added in Performance

--- performance/models.py
+++ performance/models.py
@@ -2,59 +2,80 @@ from django.db import models
 from datetime import time
 from django.utils.timezone import now  # Χρήση του timezone για timezone-aware datetime
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-from django.db.models.signals import pre_save, post_save, pre_delete
+
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.conf import settings
 
-class Performance(models.Model):
-  # Μοναδίκός αριθμός παράστασης που δεν είναι κλείδί στη βάση και τον ενημερώνει η εφαρμογή
-  performance_id = models.PositiveIntegerField(editable=False, unique=True, null=True, blank=True)
-  # Ημερομηνία δημιουργίας της παράστασης, παίρνει αυτόματα το current datetime
-  datetime = models.DateTimeField(default=now, editable=False)
-  # Τιτλος παράστασης
-  title = models.CharField(max_length=100, blank=False)
-  # Περιγραφή παράστασης
-  description = models.CharField(max_length=255, blank=False)
-  # Είδος παράστασης
-  kind = models.CharField(max_length=80, blank=False)  
-  # Διάρκεια παράστασης
-  duration = models.FloatField() 
-  # Ώρα έναρξης παράστασης
-  starting_time = models.TimeField(default=time(0, 0))  # Προεπιλογή: 00:00  
 
-  STATUSES = [
-        ('created', 'CREATED'), 
-        ('submitted', 'SUBMITTED'), 
-        ('reviewed', 'REVIEWED'), 
-        ('rejected', 'REJECTED'), 
-        ('approved', 'APPROVED'), 
+class Performance(models.Model):
+    # Μοναδίκός αριθμός παράστασης που δεν είναι κλείδί στη βάση και τον ενημερώνει η εφαρμογή
+    performance_id = models.PositiveIntegerField(editable=False, unique=True, null=True, blank=True)
+    # Ημερομηνία δημιουργίας της παράστασης, παίρνει αυτόματα το current datetime
+    datetime = models.DateTimeField(default=now, editable=False)
+    # Τίτλος παράστασης
+    title = models.CharField(max_length=100, blank=False)
+    # Περιγραφή παράστασης
+    description = models.CharField(max_length=255, blank=False)
+    # Είδος παράστασης
+    kind = models.CharField(max_length=80, blank=False)
+    # Διάρκεια παράστασης
+    duration = models.FloatField()
+    # Ώρα έναρξης παράστασης
+    starting_time = models.TimeField(default=time(0, 0))  # Προεπιλογή: 00:00
+
+    STATUSES = [
+        ('created', 'CREATED'),
+        ('submitted', 'SUBMITTED'),
+        ('reviewed', 'REVIEWED'),
+        ('rejected', 'REJECTED'),
+        ('approved', 'APPROVED'),
         ('scheduled', 'SCHEDULED')
-    ]    
-  # Το πεδίο με τις επιλογές
-  performance_status = models.CharField(
-      max_length=20,  # Το μέγιστο μήκος του string
-      choices=STATUSES,  # Οι επιλογές
-      default='created',  # Προαιρετικά, η προεπιλεγμένη επιλογή
-  )
-  
-  created_by = models.ForeignKey(
+    ]
+    # Το πεδίο με τις επιλογές
+    performance_status = models.CharField(
+        max_length=20,  # Το μέγιστο μήκος του string
+        choices=STATUSES,  # Οι επιλογές
+        default='created',  # Προαιρετικά, η προεπιλεγμένη επιλογή
+    )
+
+    created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,  # Συνδέεται με το προσαρμοσμένο μοντέλο χρήστη
         on_delete=models.CASCADE,  # Αν διαγραφεί ο χρήστης, διαγράφεται και το Performance
         related_name='performances'  # Για ευκολότερη πρόσβαση από την πλευρά του χρήστη
     )
-  
-  administrators = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='performance_admins')
 
-  def clean(self):
+  # Το πεδίο manager (επιτρέπει την επιλογή ενός χρήστη)
+    manager = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # Συνδέεται με το μοντέλο χρήστη
+        on_delete=models.SET_NULL,  # Αν διαγραφεί ο χρήστης, το πεδίο γίνεται NULL
+        null=True,  # Επιτρέπει την τιμή NULL
+        blank=True,  # Επιτρέπει να είναι προαιρετικό
+        related_name='managed_performance',  # Επιστροφή από την πλευρά του χρήστη
+    )
+
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],  # Επαλήθευση τιμών 1-5
+        default=5,
+        null=True,  # Επιτρέπει το NULL αν θέλουμε το πεδίο να είναι προαιρετικό
+        blank=True  # Επιτρέπει να μείνει κενό στη φόρμα
+    )
+
+    administrators = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='performance_admins')
+
+  
+    def clean(self):
         # Έλεγχος για ύπαρξη άλλου αντικειμένου με τον ίδιο τίτλο
         if Performance.objects.filter(title=self.title).exclude(pk=self.pk).exists():
             raise ValidationError(f"Υπάρχει ήδη παράσταση με τίτλο '{self.title}'.")
 
-  def save(self, *args, **kwargs):
-      # Εκτέλεση του clean() πριν την αποθήκευση
-      self.clean()
-      super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        # Εκτέλεση του clean() πριν την αποθήκευση
+        self.clean()
+        super().save(*args, **kwargs)
+
 
 # Σήμα για αυτόματη αύξηση του performance_id
 @receiver(pre_save, sender=Performance)
